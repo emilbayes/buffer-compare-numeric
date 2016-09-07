319085a50fbd8678195979ee247a984869db1888
Emil Bay <github@tixz.dk>
2016-09-07 13:23:13 +0930

    Add test cases with all zeros

--- test.js
+++ test.js
@@ -8,6 +8,7 @@ test('equal', function (assert) {
   assert.strictEqual(cmp(Buffer('000000AB', 'hex'), Buffer('0000AB', 'hex')), 0)
   assert.strictEqual(cmp(Buffer('000000', 'hex'), Buffer('0000', 'hex')), 0)
   assert.strictEqual(cmp(Buffer('BADA55', 'hex'), Buffer('BADA55', 'hex')), 0)
+  assert.strictEqual(cmp(Buffer('00', 'hex'), Buffer('00000000', 'hex')), 0)
   assert.end()
 })
 
@@ -17,6 +18,7 @@ test('less', function (assert) {
   assert.strictEqual(cmp(Buffer('AA', 'hex'), Buffer('FFFF', 'hex')), -1)
   assert.strictEqual(cmp(Buffer('00AA', 'hex'), Buffer('FFFF', 'hex')), -1)
   assert.strictEqual(cmp(Buffer('1337', 'hex'), Buffer('BADA55', 'hex')), -1)
+  assert.strictEqual(cmp(Buffer('00000000', 'hex'), Buffer('01', 'hex')), -1)
   assert.end()
 })
 
@@ -26,6 +28,7 @@ test('greater', function (assert) {
   assert.strictEqual(cmp(Buffer('FFFF', 'hex'), Buffer('AA', 'hex')), 1)
   assert.strictEqual(cmp(Buffer('FFFF', 'hex'), Buffer('00AA', 'hex')), 1)
   assert.strictEqual(cmp(Buffer('BADA55', 'hex'), Buffer('1337', 'hex')), 1)
+  assert.strictEqual(cmp(Buffer('01', 'hex'), Buffer('00000000', 'hex')), 1)
 
   assert.end()
 })
